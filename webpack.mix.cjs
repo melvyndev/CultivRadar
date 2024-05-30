d67dc41fd25811a0f1a42041cc78e03d3e0d5acb
melvyndev <154402936+melvyndev@users.noreply.github.com>
2024-05-30 11:06:54 +0400

    fix

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,13 +1,6 @@
 const mix = require('laravel-mix');
 
 mix.setPublicPath('public')
-   .setResourceRoot('/storage/basset/')
    .js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .version();
-
-mix.webpackConfig({
-    resolve: {
-        extensions: ['.js', '.json', '.wasm', '.mjs', '.jsx', '.scss'],
-    }
-});
