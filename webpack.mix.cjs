e830ce3f251fe45d9b8386fab2684e169bb964a5
melvyndev <154402936+melvyndev@users.noreply.github.com>
2024-05-30 19:20:32 +0400

    FIX sweat alert

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,6 +1,25 @@
 const mix = require('laravel-mix');
 
+// Définir les chemins publics et des ressources
 mix.setPublicPath('public')
-   .js('resources/js/app.js', 'public/js')
+   .setResourceRoot('/storage/basset/');
+
+// Compiler les fichiers JavaScript et Sass
+mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .version();
+
+// Configuration supplémentaire de Webpack
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.js', '.json', '.wasm', '.mjs', '.jsx', '.scss'],
+    },
+    output: {
+        publicPath: '/storage/basset/',  // Assurez-vous que le chemin de sortie est correct
+    },
+});
+
+// Inclure SweetAlert2 si nécessaire
+mix.autoload({
+    'sweetalert2': ['Swal'],
+});
